587b063d84c317be7dadebfcf876813ebbfad7ac
mehmetnurikures <97973265+mehmetnurikures@users.noreply.github.com>
2023-08-22 15:44:57 +0300

    Add files via upload

---
+++ koninin_hacmi.py
@@ -0,0 +1,7 @@
+# Bir koninin hacminin bulunmasi.
+import math
+r=int(input('koninin yaricapini cm olarak giriniz: '))
+h=int(input('koninin yuksekligini cm olarak giriniz: '))
+π=float(3.14)
+v=(1/3*π*r*r*h)
+print(v)
